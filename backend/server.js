13650236680b17a97088ddd2420712849b292f35
Sarita074 <your-github-email>
2026-03-17 16:58:21 +0530

    adding cart and analytics route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 dotenv.config();
@@ -16,9 +19,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
-
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
