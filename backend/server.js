e6f58955500e7bfa284441c08148efda65c88cb5
Sarita074 <your-github-email>
2026-04-09 11:47:12 +0530

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+
 import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
-
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-const __dirname = Path.resolve()
+const __dirname = path.resolve();
 
-app.use(express.json({ limit : "10mb"}));
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -27,15 +28,15 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-    console.log("Server is running on port http://localhost:" + PORT);
-    connectDB();
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
